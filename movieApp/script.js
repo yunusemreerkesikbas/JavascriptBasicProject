9394495b818d5e879f3456e1ef609d67877b1851
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2022-07-06 15:52:49 +0300

    base movie card added

---
+++ movieApp/script.js
@@ -0,0 +1,18 @@
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280"; 
+
+async function getMovies() {
+    const resp = await fetch(APIURL);
+    const respData = await resp.json();
+    console.log(respData);
+
+    // respData.results.forEach(movie => {
+    //     const img = document.createElement("img");
+    //     img.src = IMGPATH + movie.poster_path;
+
+    //     document.body.appendChild(img);
+    // });
+
+    return respData
+}
+getMovies();
