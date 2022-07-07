db7370347c62b1cf904e1360e629dac40ac1cc09
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2022-07-07 05:22:28 +0300

    basic drawing with mouse movement

---
+++ drawingApp/script.js
@@ -0,0 +1,36 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let size = 20;
+let isPressed = false;
+canvas.addEventListener("mousedown", () => {
+    isPressed = true;
+});
+canvas.addEventListener("mouseup", () => {
+    isPressed = false;
+});
+
+
+canvas.addEventListener("mousemove", (e) => {
+    if (isPressed) {
+        const x = e.offsetX;
+        const y = e.offsetY;
+
+        drawCircle(x, y);
+        
+    }
+});
+
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+// function draw() {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//     drawCircle(x, y);
+//     requestAnimationFrame(draw);
+// }
+// draw();
