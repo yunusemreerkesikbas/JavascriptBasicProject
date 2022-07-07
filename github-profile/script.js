406881e031585961499c7d644f3229f29449304f
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2022-07-07 03:41:11 +0300

    github user innerHTML added

---
+++ github-profile/script.js
@@ -0,0 +1,49 @@
+const APIURL = "https://api.github.com/users/";
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+async function getUser(user) {
+    const resp = await fetch(APIURL + user);
+    const respData = await resp.json();
+
+    createUserCard(respData);
+}
+
+function createUserCard(user) {
+    // const card = document.createElement('div');
+    // card.classList.add('card');
+
+    const cardHTML = `
+        <div class="card">
+            <div>
+                <img class="avatar" src="${user.avatar_url}" alt="${user.name}"> />
+            </div>
+            <div>
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+                <ul class="info">
+                    <li>${user.followers}</li>
+                    <li>${user.following}</li>
+                    <li>${user.public_repo}</li>
+
+                </ul>
+
+
+            </div>
+        </div>
+
+    `;
+    main.innerHTML = cardHTML;
+}
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    const user = search.value;
+    if (user) {
+        getUser(user);
+
+        search.value = "";
+    }
+
+})
