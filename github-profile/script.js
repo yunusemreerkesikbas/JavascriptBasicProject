ab4807002e4845c27a12583226c70530a5a60477
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2022-07-07 04:30:36 +0300

    added repos sorting by stars attributes

--- github-profile/script.js
+++ github-profile/script.js
@@ -3,7 +3,7 @@ const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
-getUser("florinpop17")
+getUser("yunusemreerkesikbas")
 
 async function getUser(username) {
     const resp = await fetch(APIURL + username);
@@ -14,7 +14,7 @@ async function getUser(username) {
     getRepos(username);
 }
 async function getRepos(username) {
-    const resp = await fetch(APIURL + username + 'repos');
+    const resp = await fetch(APIURL + username + '/repos');
     const respData = await resp.json();
     addReposToCard(respData);
 
@@ -26,7 +26,7 @@ function createUserCard(user) {
 
     const cardHTML = `
         <div class="card">
-            <div class="img-container">
+            <div >
                 <img class="avatar" src="${user.avatar_url}" alt="${user.name}"> 
             </div>
             <div class="user-info">
@@ -39,27 +39,28 @@ function createUserCard(user) {
 
                 </ul>
                 <div id="repos"></div>
-
-
             </div>
         </div>
 
     `;
     main.innerHTML = cardHTML;
 }
-function addReposToCard(params) {
+function addReposToCard(repos) {
     const reposEl = document.getElementById('repos');
-    repos.forEach(repo => {
-        const repoEl = document.createElement('a');
-        repoEl.classList.add('repo');
-        repoEl.href = repo.html_url;
-        repoEl.target = "_blank";
-        repoEl.innerText = repo.name;
-        reposEl.appendChild(repoEl);
-    });
+    repos
+        .sort((a, b) => b.stargazers_count - a.stargazers_count)
+        .slice(0, 10)
+        .forEach(repo => {
+            const repoEl = document.createElement('a');
+            repoEl.classList.add('repo');
+            repoEl.href = repo.html_url;
+            repoEl.target = "_blank";
+            repoEl.innerText = repo.name;
+            reposEl.appendChild(repoEl);
+        });
 }
 
-form.addEventListener("submit", (e) =>{
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     const user = search.value;
     if (user) {
